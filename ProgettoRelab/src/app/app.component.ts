7c3a32acbe3284764668afcae3d63b7112c1d671
gioele-bra <brancaleoni.gioele@iisgalvanimi.edu.it>
2020-05-23 10:01:10 +0000

    Esercizio 04.2 | Done

--- ProgettoRelab/src/app/app.component.ts
+++ ProgettoRelab/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Marker } from './models/marker.model';
 import { Ci_vettore } from './models/ci_vett.model';
+import { MouseEvent } from '@agm/core';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +22,10 @@ export class AppComponent implements OnInit {
   lat: number = 45.45227445505016;
   obsCiVett : Observable<Ci_vettore[]>; //Crea un observable per ricevere i vettori energetici
   markers : Marker[] //Marker va importato
+  circleLat : number = 0; //Latitudine e longitudine iniziale del cerchio
+  circleLng: number = 0;
+  maxRadius: number = 400; //Voglio evitare raggi troppo grossi
+  radius : number = this.maxRadius; //Memorizzo il raggio del cerchio
 
   constructor(public http: HttpClient) {
   }
@@ -48,26 +53,66 @@ export class AppComponent implements OnInit {
       lngTot += m.lng;
       this.markers.push(m);
     }
-    this.lng = lngTot/data.length; //Commenta qui
+    this.lng = lngTot/data.length; //Si fa la media delle longitudini e latitudini, per stabilire il centro della zona
     this.lat = latTot/data.length;
     this.zoom = 16;
     }
  }
 
+ mapClicked($event: MouseEvent) {
+    this.circleLat = $event.coords.lat; //Queste sono le coordinate cliccate
+    this.circleLng = $event.coords.lng; //Sposto il centro del cerchio qui
+    this.lat = this.circleLat; //Sposto il centro della mappa qui
+    this.lng = this.circleLng;
+    this.zoom = 15;  //Zoom sul cerchio
+  }
+
   ngOnInit() {
-    this.obsGeoData = this.http.get<GeoFeatureCollection>('https://3000-b5234404-990e-49ae-a442-0feb58cb83d5.ws-eu01.gitpod.io/');
+    this.obsGeoData = this.http.get<GeoFeatureCollection>('https://3000-ff928705-26b4-41f1-956a-9e82ec83b595.ws-eu01.gitpod.io/');
     this.obsGeoData.subscribe(this.prepareData);
-    //Effettua la chiamatata al server per ottenere l’elenco dei vettori energetici
-    this.obsCiVett = this.http.get<Ci_vettore[]>("https://3000-b5234404-990e-49ae-a442-0feb58cb83d5.ws-eu01.gitpod.io/ci_vettore/89");
+  }
+
+  circleRedim(newRadius : number){
+    console.log(newRadius) //posso leggere sulla console il nuovo raggio
+    this.radius = newRadius;  //Ogni volta che modifico il cerchio, ne salvo il raggio
+  }
+
+  circleDoubleClicked(circleCenter)
+  {
+    console.log(circleCenter); //Voglio ottenere solo i valori entro questo cerchio
+    console.log(this.radius);
+
+    this.circleLat = circleCenter.coords.lat; //Aggiorno le coordinate del cerchio
+    this.circleLng = circleCenter.coords.lng; //Aggiorno le coordinate del cerchio
+
+    //Non conosco ancora le prestazioni del DB, non voglio fare ricerche troppo onerose
+    if(this.radius > this.maxRadius)
+    {
+      console.log("area selezionata troppo vasta sarà reimpostata a maxRadius");
+       this.radius = this.maxRadius;
+    }
+    console.log ("raggio in gradi " + (this.radius * 0.00001)/1.1132)
+
+    //Voglio spedire al server una richiesta che mi ritorni tutte le abitazioni all'interno del cerchio
+
+    let raggioInGradi = (this.radius * 0.00001)/1.1132;
+    //Posso riusare lo stesso observable e lo stesso metodo di gestione del metodo
+    //cambiaFoglio poichè riceverò lo stesso tipo di dati
+    //Divido l'url andando a capo per questioni di leggibilità non perchè sia necessario
+    this.obsCiVett = this.http.get<Ci_vettore[]>(`https://3000-ff928705-26b4-41f1-956a-9e82ec83b595.ws-eu01.gitpod.io/ci_geovettore/
+    ${this.circleLat}/
+    ${this.circleLng}/
+    ${raggioInGradi}`);
     this.obsCiVett.subscribe(this.prepareCiVettData);
+
   }
 
   //Questo metodo richiama la route sul server che recupera il foglio specificato nella casella di testo
   cambiaFoglio(foglio) : boolean
   {
-    let val = foglio.value; //Commenta qui
-    this.obsCiVett = this.http.get<Ci_vettore[]>(`TUO_URL/ci_vettore/${val}`);  //Commenta qui
-    this.obsCiVett.subscribe(this.prepareCiVettData); //Commenta qui
+    let val = foglio.value; // prendo il valore da un componente html, come foglio che è un input tag
+    this.obsCiVett = this.http.get<Ci_vettore[]>(`https://3000-ff928705-26b4-41f1-956a-9e82ec83b595.ws-eu01.gitpod.io/ci_vettore/${val}`);  //prendo i dati del foglio scelto dal sito dove è hostato il server
+    this.obsCiVett.subscribe(this.prepareCiVettData); //Invio i dati ottenuti all'observable che li aspetta. Questo modo di agire è detto asincrono
     console.log(val);
     return false;
   }
